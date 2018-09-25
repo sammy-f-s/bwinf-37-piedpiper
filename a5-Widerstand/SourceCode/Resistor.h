be9a436be659f1cc8e39926186d502fbf0725d37
Jerrit Gläsker <jerritglaesker@gmail.com>
2018-09-25 16:27:33 +0000

    Resistor-Class

---
+++ a5-Widerstand/SourceCode/Resistor.h
@@ -0,0 +1,23 @@
+#pragma once
+
+enum WiringType
+{
+	Serial,
+	Parallel,
+	None
+};
+
+struct Resistor{
+	float ohm;
+	WiringType wtype;
+
+	Resistor()
+	{
+		ohm = 0;
+		wtype = None;
+	}
+	Resistor(float o, WiringType w){
+		ohm = o;
+		wtype = w;
+	}
+};
