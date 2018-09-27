9ec8ea7f4c5b3d583ca6e39ca06ee01dbda25302
cherrypicking <jerritglaesker@gmail.com>
2018-09-27 17:17:12 +0200

    Option beliebig viele Versuche mit zuf. Zahlen zu machen

--- a3-Voll_daneben/SourceCode/Casino.cpp
+++ a3-Voll_daneben/SourceCode/Casino.cpp
@@ -1,6 +1,9 @@
 // Casino.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
 //
 
+#define PBSTR "============================================================"
+#define PBWIDTH 40
+
 #include "stdafx.h"
 
 #include <vector>
@@ -8,10 +11,14 @@
 #include <iostream>
 #include <string>
 #include <algorithm> //std::sort
+#include <time.h> //time(NULL)
 
 using namespace std;
 
 float averageInRange(vector <int> &entries, int low, int high);
+float evaluate(vector <int> &entries);
+void printProgress(double percentage);
+void fillRandomly(vector <int> &vec);
 int getClosest(int a, vector <int> &picks);
 float toPay(vector <int> &entries, vector <int> &picks);
 
@@ -21,27 +28,101 @@ int main()
 	vector <int> entries;
 	vector <int> picks;
 
+	srand(time(NULL));
+
 	int nr;
-	cout << "Beispiel Nr.?" << endl;
+	cout << "Beispiel Nr.?" << "    (0 für zufällige Zahlen)" << endl;
 	cin >> nr;
 	cout << "-----------------" << endl;
 
-	ifstream stream("beispiel" + to_string(nr) + ".txt");
-	string line;
-
-	if (stream.is_open())
+	if (nr != 0)
 	{
-		while (getline(stream, line))
+		ifstream stream("beispiel" + to_string(nr) + ".txt");
+		string line;
+
+		if (stream.is_open())
 		{
-			entries.push_back(stoi(line));
+			while (getline(stream, line))
+			{
+				entries.push_back(stoi(line));
+			}
 		}
+		else
+		{
+			cerr << "File not found" << endl;
+			system("PAUSE");
+			return 0;
+		}
+
+		cout << "Anfangseinnahmen: " << entries.size() * 25 << endl << endl;
+
+		cout << "Al Capones Wahl: " << endl;
+		for (int i = 0; i < 10; i++)
+		{
+			//Unterteilung von 1000 in 10 gleichgroße Bereiche, mit dem Marker in der Mitte
+			//Start des Markers in der Mitte seines Bereiches
+			int marker = i * 100 + 50;
+
+			//Um möglichst dicht an möglichst vielen Leuten zu liegen wird die durchschnittlich gewählte Zahl in diesem Bereich errechnet
+			float avInRange = averageInRange(entries, marker - 50, marker + 50);
+			cout << (int)avInRange << ", ";
+
+			picks.push_back((int)avInRange);
+		}
+
+		cout << endl << endl;
+		cout << "Auszuzahlen: " << toPay(entries, picks) << endl;
+	}
+	else
+	{
+		cout << "Durchläufe?" << endl;
+		int iterations;
+		cin >> iterations;
+
+		float dGewinn = 0;
+		for (int i = 0; i < iterations; i++)
+		{
+			printProgress((float)i / (float)iterations);
+			entries.clear();
+			fillRandomly(entries);
+			float einzelGewinn = evaluate(entries);
+			//cout << einzelGewinn << endl;
+			dGewinn += einzelGewinn;
+		}
+
+		dGewinn /= iterations;
+
+		cout << endl << "Durchschnittlicher Gewinn: " << dGewinn << endl;
 	}
 
-	cout << "Anfangseinnahmen: " << entries.size() * 25 << endl << endl;
+	system("PAUSE");
+	return 0;
+}
 
-	sort(entries.begin(), entries.end());
+void printProgress(double p)
+{
+	int val = (int)(p * 100);
+	int left = (int)(p * PBWIDTH);
+	int right = PBWIDTH - left;
+	printf("\r%3d%% [%.*s%*s]", val, left, PBSTR, right, "");
+	fflush(stdout);
+}
 
-	cout << "Al Capones Wahl: " << endl;
+void fillRandomly(vector <int> &vec)
+{
+	for (int i = 0; i < 100; i++)
+	{
+		vec.push_back(rand() % 1000 + 1);
+	}
+}
+
+//zur Quantifizierun (bilden des durchschnittlichen Gewinns)
+float evaluate(vector <int> &entries)
+{
+	vector <int> picks;
+	
+	float anfangsEinnahmen = entries.size() * 25;
+
 	for (int i = 0; i < 10; i++)
 	{
 		//Unterteilung von 1000 in 10 gleichgroße Bereiche, mit dem Marker in der Mitte
@@ -50,16 +131,14 @@ int main()
 
 		//Um möglichst dicht an möglichst vielen Leuten zu liegen wird die durchschnittlich gewählte Zahl in diesem Bereich errechnet
 		float avInRange = averageInRange(entries, marker - 50, marker + 50);
-		cout << (int)avInRange << ", ";
 
 		picks.push_back((int)avInRange);
 	}
 
-	cout << endl << endl;
-	cout << "Auszuzahlen: " << toPay(entries, picks) << endl;
+	float zuZahlen = toPay(entries, picks);
+	float gewinn = anfangsEinnahmen - zuZahlen;
 
-	system("PAUSE");
-    return 0;
+	return gewinn;
 }
 
 float toPay(vector <int> &entries, vector <int> &picks)
